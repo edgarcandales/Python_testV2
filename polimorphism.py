41e244763fd42d097db8ee79224f2310c4a656fd
edgarcandales <68811563+edgarcandales@users.noreply.github.com>
2021-02-13 17:36:47 -0500

    Testing Python chains and Polymorphisms

---
+++ polimorphism.py
@@ -0,0 +1,40 @@
+class Coche():
+
+    def desplazamiento(self):
+        print("Me desplazo untilizando cuatro ruedas")
+
+
+class Moto():
+
+    def desplazamiento(self):
+        print("Me desplazo utilizando dos ruedas")
+
+
+class Camion():
+
+    def desplazamiento(self):
+        print("Me desplazo utilizando seis ruedas")
+
+
+#miVehiculo=Moto()
+
+#miVehiculo.desplazamiento()
+
+#miVehiculo2=Coche()
+
+#miVehiculo2.desplazamiento()
+
+#miVehiculo3=Camion()
+
+#miVehiculo3.desplazamiento()
+
+def desplazamientoVehiculo(vehiculo):
+    vehiculo.desplazamiento()
+
+miVehiculo=Camion()
+
+desplazamientoVehiculo(miVehiculo)
+
+
+
+
